6080ca8c6972840ac647e181758511f787a0bded
Alin Salagean <slgnalindev@gmail.com>
2024-03-17 15:38:53 +0200

    Add main problem for subsets

--- subsets/main.cpp
+++ subsets/main.cpp
@@ -1,60 +1,25 @@
 /**
  * ==========================================================================================================
- *
- * Se dau n multimi, ca cele de mai jos
- *
- * S(1) = {1, 2, ... ,w(1)}
- * S(2) = {1, 2, ... ,w(2)}
- *
- * ...
- *
- * S(n) = {1, 2, ... ,w(n)}
- *
- * Sa se genereze produsul lor cartezian
- *
- * ==========================================================================================================
- *
- * Date de intrare
- *
- * Fisierul input.txt contine pe primul rand valoare numarului `n`, reprezentand numarul de multimi
- * Pe urmatoarele `n` randuri ale fisierului sunt definite valorile w(x), reprezentand numarul de elemente din fiecare multime
+ * Fie `n` - numar natural nenul
+ * Sa se genereze toate submultimile multimii S = {1, 2, ..., n}
  *
  * ==========================================================================================================
  *
  * Exemplu:
  *
- * Pentru urmatoarele date de intrare
- * n = 3,
- * w(1) = 2
- * w(2) = 3
- * w(3) = 2
+ * Pentru n = 3
+ * S = {1, 2, 3}
  *
- * Fisierul input.txt este definit ca
- * ````````````````````````
- * 3
- * 2
- * 3
- * 2
- * ````````````````````````
+ * Submultimile sunt urmatoarele:
  *
- * n = 3
- * w[0] = 2
- * w[1] = 3
- * w[2] = 2
- *
- * (1, 1, 1)
- * (1, 1, 2)
- * (1, 2, 1)
- * (1, 2, 2)
- * (1, 3, 1)
- * (1, 3, 2)
- * (2, 1, 1)
- * (2, 1, 2)
- * (2, 2, 1)
- * (2, 2, 2)
- * (2, 3, 1)
- * (2, 3, 2)
- *
+ * Multimea vida
+ * {1}
+ * {2}
+ * {3}
+ * {1, 2}
+ * {1, 3}
+ * {2, 3}
+ * {1, 2, 3}
  *
  * ==========================================================================================================
  * */
